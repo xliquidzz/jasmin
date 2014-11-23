15ed82080fe2943a4dae9b6efdad2408f9b45a77
Marcus Fihlon <marcus@fihlon.ch>
2014-11-23 13:47:27 +0100

    #11 Backend: Update a Sprint

--- src/main/java/ch/fihlon/jasmin/resources/SprintResource.java
+++ src/main/java/ch/fihlon/jasmin/resources/SprintResource.java
@@ -8,6 +8,7 @@ import javax.annotation.Nonnull;
 import javax.validation.Valid;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -41,5 +42,15 @@ public class SprintResource {
             return Response.status(Response.Status.NOT_FOUND).build();
         }
         return Response.ok(sprint).build();
+    }
+
+    @PUT
+    @Path("/{id}")
+    public Response updateSprint(@PathParam("id") final long id, @Valid final Sprint sprint) throws URISyntaxException {
+        if (sprintDAO.readSprintById(id) == null) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+        sprintDAO.updateSprint(id, sprint.getTitle(), Date.valueOf(sprint.getStart()), Date.valueOf(sprint.getEnd()));
+        return Response.ok(new Sprint(id, sprint.getTitle(), sprint.getStart(), sprint.getEnd())).build();
     }
 }
